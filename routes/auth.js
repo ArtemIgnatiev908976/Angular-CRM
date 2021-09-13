e2ff11728a7500f748925acabdf286ca5b869b4d
Artem <ignatev.artem.nik@gmail.com>
2021-09-13 13:57:45 +0400

    создание всех роутов

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,11 @@ const express = require('express')
 const controller = require('../controllers/auth')
 const router = express.Router()
 
+// localhost:5000/api/auth/login
+router.post('/login', controller.login)
 
-//localhost:5000/api/auth/login
+// localhost:5000/api/auth/register
+router.post('/register', controller.register)
 
-router.get('/login', controller.login)
 
-//localhost:5000/api/auth/register
-router.get('/register', controller.register)
-
-module.exports =router
+module.exports = router
