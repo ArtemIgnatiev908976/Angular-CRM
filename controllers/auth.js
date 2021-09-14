dc3ae376a8d6de191a281294b51db6a99c581284
Artem <ignatev.artem.nik@gmail.com>
2021-09-14 15:22:28 +0400

    Универсальная обработка ошибок утилитой

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const jwt =require('jsonwebtoken')
 const User = require('../models/User')
 const keys = require('../config/keys')
 
+const errorHandler = require('../utils/errorHandler')
 module.exports.login = async function (req, res) {
     const candidate = await User.findOne({email: req.body.email})
     if (candidate) {
@@ -55,6 +56,7 @@ module.exports.register = async function (req, res) {
             res.status(201).json(user)
         } catch (e) {
             //Обработать ошибку
+            errorHandler(res, e)
         }
 
     }
