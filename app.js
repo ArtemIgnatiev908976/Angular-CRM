1eca51a73d7b057d4a1ff071151859180d0a9f8b
Artem <ignatev.artem.nik@gmail.com>
2021-09-14 14:57:34 +0400

    Защита роутов с Passport.js

--- app.js
+++ app.js
@@ -8,10 +8,18 @@ const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
 const keys = require('./config/keys')
 const app = express()
+const passport = require('passport')
+
 
 mongoose.connect(keys.mongoURI)
   .then(() => console.log('MongoDB connected.'))
   .catch(error => console.log(error))
+
+
+app.use(passport.initialize())
+
+require('./middleware/passport')(passport)
+
 
 app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
