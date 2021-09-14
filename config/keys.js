c03df5424a9976e474e7f8b0edca35ffcbb5c134
Artem <ignatev.artem.nik@gmail.com>
2021-09-14 13:49:23 +0400

    Логин в систему, генерация токена через jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    mongoURI:'mongodb+srv://Artem:Qazwsxedc908976@cluster0.yyixn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://Artem:Qazwsxedc908976@cluster0.yyixn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
